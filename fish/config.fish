c1067abca2d282542f38d66fffb899b1e828989b
Ericdarkblade <ezmoswalker@gmail.com>
2024-03-04 16:56:43 -0500

    fish changes

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,17 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    alias vi='nvim'
+    alias vim='nvim'
+    alias open='wslview'
+    alias pwsh='pwsh.exe'
+
+    export BROWSER=wslview
+
+    #alias oldvim ='/usr/bin/vim'
+    function oldvim --wraps =/usr/bin/vim --description 'alias oldvim =/usr/bin/vim';  /usr/bin/vim $argv; end
 
     fish_vi_key_bindings
     
-    alias vim='nvim'
-    alias pwsh='pwsh.exe'
-
     bind --mode insert --sets-mode default \cc repaint
     #bind -M insert \cc 'set fish_bind_mode default; commandline -f repaint'
 
@@ -16,3 +22,6 @@ if status is-interactive
         nvim -c "Man $manual | only"
     end
 end
+
+# Created by `pipx` on 2024-02-15 22:21:53
+set PATH $PATH /home/eric/.local/bin
