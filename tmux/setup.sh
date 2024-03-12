5430cbe52c810ca67e50fc7e36290805b5da4439
Ericdarkblade <ezmoswalker@gmail.com>
2024-03-12 13:27:07 -0400

    modified tmux setup

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm;
 
+
+curl https://raw.githubusercontent.com/Ericdarkblade/.saved-configs/main/tmux/.tmux.conf > ~/.tmux.conf
